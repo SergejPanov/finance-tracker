d9992337fab13e52b1b82d3447679d8fa4aaf6e6
Sergey Panov <Sergej.Panov@gmail.com>
2021-01-17 17:22:43 +0100

    Lots of changes done

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,17 +1,24 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
 
-    def self.new_lookup(ticker_symbol)
+  validates :name, :ticker, presence: true
+
+    def self.new_lookup(ticker)
         client = IEX::Api::Client.new(
             publishable_token: Rails.application.credentials.iex[:pub_token],
             secret_token: Rails.application.credentials.iex[:sec_token],
             endpoint: 'https://sandbox.iexapis.com/v1'
           )
           begin 
-            new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price:client.price(ticker_symbol))
+            new(ticker: ticker, name: client.company(ticker).company_name, last_price:client.price(ticker))
           rescue => exception
             return nil
           end
 
-        end
-
+    end
+     
+    def self.check_db(ticker)
+      where(ticker: ticker).first
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,28 @@
 class User < ApplicationRecord
+  has_many :user_stocks
+  has_many :stocks, through: :user_stocks
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+def stock_already_tracked?(ticker)
+  stock = Stock.check_db(ticker)
+  return false unless stock
+  stocks.where(id: stock.id).exists?
 end
+
+def under_stock_limit?
+  stocks.count < 3
+end
+
+         def can_track_stock?(ticker)
+            under_stock_limit? && !stock_already_tracked?(ticker)
+         end
+
+         def full_name
+            return "#{first_name} #{last_name}" if first_name || last_name
+            return "Anonymous"
+         end
+end
+
